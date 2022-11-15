4173b6fb745cca8dcfced83a04db9b11a35c55e6
Andres Zubizarreta <113726288+andreszubi@users.noreply.github.com>
2022-11-15 13:26:21 +0100

    Iteration 3 done

---
+++ src/components/Random.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Random({min, max}) {
+    const randomNum = Math.floor(Math.random() * (max - min) + min);
+    return (
+        <div>
+            <p>Random value between {min} and {max} => {randomNum}</p>
+        </div>
+    )
+}
+
+export default Random;
